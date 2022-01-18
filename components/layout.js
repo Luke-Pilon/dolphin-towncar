02949a95f7fcdff5af1fb743ab1ba8ec054be318
= <=>
2022-01-18 14:37:41 -0600

    adding global meta tags in layout component and page-specific tags in index page

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,18 @@
+import Head from 'next/head';
 import Header from './header';
 import Footer from './footer';
 
 export default function Layout({ children }) {
     return (
         <>
+            <Head>
+                <meta charset='UTF-8' />
+                <meta
+                    name='viewport'
+                    content='initial-scale=1.0, width=device-width'
+                />
+                <link rel='icon' href='/logo.png' />
+            </Head>
             <Header />
             <main>{children}</main>
             <Footer />
