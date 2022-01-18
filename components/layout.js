4dca7e8ceb7058d8296f0d0e4780b7124aec427d
= <=>
2022-01-18 14:51:11 -0600

    implementing custom _document and _app pages for global meta tags

--- components/layout.js
+++ components/layout.js
@@ -5,14 +5,6 @@ import Footer from './footer';
 export default function Layout({ children }) {
     return (
         <>
-            <Head>
-                <meta charset='UTF-8' />
-                <meta
-                    name='viewport'
-                    content='initial-scale=1.0, width=device-width'
-                />
-                <link rel='icon' href='/logo.png' />
-            </Head>
             <Header />
             <main>{children}</main>
             <Footer />
