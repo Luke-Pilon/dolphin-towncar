9edba9133aee56de53b874c9d35f3448c220f090
= <=>
2022-01-19 15:55:49 -0600

    implementing and styling bottom navigation container

---
+++ components/bottom-nav.js
@@ -0,0 +1,6 @@
+import Link from 'next/link';
+import styles from './bottom-nav.module.css';
+
+export default function BottomNav() {
+    return <nav className={styles.nav}></nav>;
+}
--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,14 @@
 import Head from 'next/head';
 import Header from './header';
 import Footer from './footer';
+import BottomNav from './bottom-nav';
 
 export default function Layout({ children }) {
     return (
         <>
             <Header />
             <main>{children}</main>
+            <BottomNav />
             <Footer />
         </>
     );
