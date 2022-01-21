669fb8ad326a55ea4de8af4cfd2ba37511b04a13
= <=>
2022-01-21 17:31:26 -0600

    adding links and styling for bottom nav

--- components/bottom-nav.js
+++ components/bottom-nav.js
@@ -2,5 +2,29 @@ import Link from 'next/link';
 import styles from './bottom-nav.module.css';
 
 export default function BottomNav() {
-    return <nav className={styles.nav}></nav>;
+    return (
+        <nav className={styles.nav} role='navigation'>
+            <ul className={styles.linkList}>
+                <li className={styles.linkListItem}>
+                    <Link href='/quote'>
+                        <a className={styles.link}>
+                            <span>Quote</span>
+                        </a>
+                    </Link>
+                </li>
+                <li className={styles.linkListItem}>
+                    <a href='tel:952-201-9725' className={styles.link}>
+                        <span>Call</span>
+                    </a>
+                </li>
+                <li className={styles.linkListItem}>
+                    <Link href='/rates'>
+                        <a className={styles.link}>
+                            <span>Rates</span>
+                        </a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
