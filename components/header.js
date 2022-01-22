75a637ecd52e37a3d01cd8c471516af9d211bebe
= <=>
2022-01-21 23:15:14 -0600

    implementing togglable side nav with hamburger icon

--- components/header.js
+++ components/header.js
@@ -1,9 +1,36 @@
 import Link from 'next/link';
+import Script from 'next/script';
+import { useEffect } from 'react';
+import styles from './header.module.css';
 
 export default function Header() {
+    useEffect(() => {
+        document.getElementById('toggle').addEventListener('click', () => {
+            const sidebar = document.getElementById('sidebar');
+            console.log('toggled');
+            sidebar.style.left = sidebar.style.left === '0px' ? '-100%' : '0px';
+        });
+    }, []);
     return (
-        <header>
-            <nav></nav>
+        <header className={styles.header} id='header'>
+            <nav className={styles.nav}>
+                <div className={styles.sidebarToggle}>
+                    <input
+                        type='checkbox'
+                        className={styles.toggler}
+                        id='toggle'
+                    />
+                    <div className={styles.hamburger}>
+                        <div></div>
+                    </div>
+                </div>
+                <div className={styles.brandLinkItem}>
+                    <Link href='/'>
+                        <a className={styles.brandLink}>Dolphin Limo LLC</a>
+                    </Link>
+                </div>
+            </nav>
+            <div className={styles.sidebar} id='sidebar'></div>
         </header>
     );
 }
