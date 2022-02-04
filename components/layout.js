c6dda8b6298ef8cca3c291e0f0a447602e5460d7
= <=>
2022-02-03 20:10:09 -0600

    removing page files - transition to single page app

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,15 @@
-import Head from 'next/head';
 import Header from './header';
 import Footer from './footer';
 import BottomNav from './bottom-nav';
 
+//TODO move to footer component
+//Use the <div> to allow scrolling to the bottom of the content with the sticky navbar on the bottom of the page
 export default function Layout({ children }) {
     return (
         <>
             <Header />
             <main>{children}</main>
+            <div style={{ clear: 'both', height: '65px' }}></div>
             <BottomNav />
             <Footer />
         </>
