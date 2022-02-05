527283dc70796639fc270a5ad5f3a8c2ffd9b066
= <=>
2022-02-05 15:29:31 -0600

    adding markup and styling for rates table

--- components/rates.js
+++ components/rates.js
@@ -1,22 +1,29 @@
+import Quote from './quote';
 import styles from './rates.module.css';
 
 export default function Rates({ rates }) {
     return (
         <>
+            <h3>Rates</h3>
             <table className={styles.ratesTable}>
                 <thead>
-                    <th>From</th>
-                    <th>To</th>
-                    <th>Cost</th>
-                </thead>
-                {rates.map((rate, i) => (
-                    <tr key={i}>
-                        <td>{rate.from}</td>
-                        <td>{rate.to}</td>
-                        <td>{rate.cost}</td>
+                    <tr>
+                        <th className={styles.tableCell}>From</th>
+                        <th className={styles.tableCell}>To</th>
+                        <th className={styles.tableCell}>Cost</th>
                     </tr>
-                ))}
+                </thead>
+                <tbody>
+                    {rates.map((rate, i) => (
+                        <tr key={i}>
+                            <td className={styles.tableCell}>{rate.from}</td>
+                            <td className={styles.tableCell}>{rate.to}</td>
+                            <td className={styles.tableCell}>{rate.cost}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
+            <Quote />
         </>
     );
 }
