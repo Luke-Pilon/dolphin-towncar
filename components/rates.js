4cd3defd1ad44da19aa8c891b9fa7635364eed6c
= <=>
2022-02-04 22:51:02 -0600

    adding table of rates in the rate component

--- components/rates.js
+++ components/rates.js
@@ -1,3 +1,22 @@
-export default function Rates() {
-    return <></>;
+import styles from './rates.module.css';
+
+export default function Rates({ rates }) {
+    return (
+        <>
+            <table className={styles.ratesTable}>
+                <thead>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Cost</th>
+                </thead>
+                {rates.map((rate, i) => (
+                    <tr key={i}>
+                        <td>{rate.from}</td>
+                        <td>{rate.to}</td>
+                        <td>{rate.cost}</td>
+                    </tr>
+                ))}
+            </table>
+        </>
+    );
 }
