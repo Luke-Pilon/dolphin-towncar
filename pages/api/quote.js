c3ffb41771657760c311d980ce4eb0901f331fe3
= <=>
2022-02-10 15:07:16 -0600

    "adding todo (twilio messaging implementation)"

--- pages/api/quote.js
+++ pages/api/quote.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
+        //TODO implement twilio messaging
         res.status(200).json({ successful: true });
     }
 }
