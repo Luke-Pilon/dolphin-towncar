3534aff1a3a97f817c0a51cb531281958687aba9
= <=>
2022-02-12 21:43:16 -0600

    implementing twilio for messaging

--- pages/api/quote.js
+++ pages/api/quote.js
@@ -1,6 +1,18 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
-        //TODO implement twilio messaging
+        const accountSid = process.env.TWILIO_ACCOUNT_SID;
+        const authToken = process.env.TWILIO_AUTH_TOKEN;
+        const from = process.env.TWILIO_PHONE_NUMBER;
+        const client = require('twilio')(accountSid, authToken);
+        /*
+        client.messages
+            .create({
+                body: 'testing',
+                from: from,
+                to: '+16512311655',
+            })
+            .then((message) => console.log(message.status));
+            */
         res.status(200).json({ successful: true });
     }
 }
