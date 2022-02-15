927c173181b69831831df893a44084d0e6187900
= <=>
2022-02-14 20:31:33 -0600

    fixing rendering behavior of header component (server vs client side)

--- components/header.js
+++ components/header.js
@@ -1,10 +1,12 @@
+import { useEffect } from 'react';
 import DesktopHeader from './desktop-header';
 import MobileHeader from './mobile-header';
+import { useState } from 'react';
 
 export default function Header() {
-    return typeof window !== 'undefined' && window.innerWidth < 768 ? (
-        <MobileHeader />
-    ) : (
-        <DesktopHeader />
-    );
+    const [isMobile, setIsMobile] = useState();
+    useEffect(() => {
+        setIsMobile(window.innerWidth < 768);
+    }, []);
+    return isMobile ? <MobileHeader /> : <DesktopHeader />;
 }
